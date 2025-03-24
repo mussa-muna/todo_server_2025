396185de2f14662a3010aad4c60657d3ae74d13e
Mussa Muna <munammahadi@gmail.com>
2025-03-24 11:56:35 +0200

    fix: update CORS configuration to allow client domain

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const todoRouter = require("./routes/todo");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+// Configure CORS
+const corsOptions = {
+    origin: ['https://todo-client-2025.onrender.com', 'http://localhost:5500'],
+    methods: ['GET', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", todoRouter);
 
